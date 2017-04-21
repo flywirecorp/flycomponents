e119c95ac89cc8589129c1e6736bbad41531f1fd
Vicent Gozalbes <vigosan@gmail.com>
2017-04-21 10:26:14 +0200

    Flycomponents example

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,103 @@
 var webpack = require('webpack')
 var path = require('path')
+var settings = {
+  isDevelopment: process.env.NODE_ENV !== 'production',
+  ip: process.env.IP || '0.0.0.0',
+  port: process.env.PORT || 8080
+}
 
-module.exports = {
-  context: __dirname,
-  entry: {
-    main: path.join(__dirname, 'src', 'index.js')
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'flycomponents.js',
-    library:'flycomponents',
-    libraryTarget:'var'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
+function buildConfig (options) {
+  return {
+    context: __dirname,
+    entry: {
+      main: path.join(__dirname, 'src', 'index.js')
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'flycomponents.js',
+      libraryTarget: 'umd',
+      library: 'flycomponents'
+    },
+    devServer: {
+      inline: true,
+      host: options.ip,
+      hot: true,
+      noInfo: false,
+      port: options.port,
+      stats: {
+        colors: true,
+        reasons: true,
+        chunks: true
       }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-      }
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.AggressiveMergingPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        use: 'eslint-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [require('autoprefixer')]
-              }
-            }
-          },
-          {
-            loader: 'sass-loader'
-          }
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        }
+      })
+    ].concat(
+      options.isDevelopment
+        ? [
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.NoEmitOnErrorsPlugin()
         ]
-      },
-    ]
+          : [
+            new webpack.optimize.UglifyJsPlugin({
+              compress: {
+                warnings: false,
+                screw_ie8: true
+              },
+              output: {
+                comments: false,
+              }
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.AggressiveMergingPlugin(),
+          ]
+    ),
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          use: 'eslint-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [require('autoprefixer')]
+                }
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ]
+    }
   }
 }
+
+module.exports = (function (options) {
+  return buildConfig(options)
+})(settings)
