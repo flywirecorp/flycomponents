3300b1cc5b5dbf8f20d224ce5057bc7c0cfff498
Alvaro Bautista <alvarobp@gmail.com>
2017-06-01 13:57:28 +0200

    Make TextInput pass only the props supported by its children
    Warnings where shown in the console because it was passing props
    not supported by the underlying components. For example: when sufix
    was passed the <input /> component underneath was getting that prop.

--- docs/src/components/TextInput/TextInput.js
+++ docs/src/components/TextInput/TextInput.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import { TextInput } from '../../../../src'
+import { TextInput, FormGroup } from '../../../../src'
 import Component from '../Component'
 import README from './README.md'
 
 export default () => (
   <Component readme={README}>
-    <TextInput name="twitter" prefix="@" />
+    <FormGroup name="default" label="Default" className="FormGroup GridColumn">
+      <TextInput name="default" />
+    </FormGroup>
+    <FormGroup
+      name="withprefix"
+      label="With prefix"
+      className="FormGroup GridColumn"
+    >
+      <TextInput name="withprefix" prefix="@" />
+    </FormGroup>
+    <FormGroup
+      name="withsufix"
+      label="With sufix"
+      className="FormGroup GridColumn"
+    >
+      <TextInput name="withsufix" sufix=".com" />
+    </FormGroup>
+    <FormGroup
+      name="withvalue"
+      label="With value"
+      className="FormGroup GridColumn"
+    >
+      <TextInput name="withvalue" value="Preset value" />
+    </FormGroup>
+    <FormGroup
+      name="multiline"
+      label="Multiline"
+      className="FormGroup GridColumn"
+    >
+      <TextInput name="multiline" multiline={true} />
+    </FormGroup>
   </Component>
 )
