7d9db5ed9cf79b3c6ea7544f94282eb28a7db140
Javier <javier.arques@peertransfer.com>
2017-06-20 17:24:41 +0200

    Disable html autocomplete (#25)
    * Move shared Markdown component to /src folder
    
    * Disable HTML autocomplete in Autocomplete input

--- docs/src/components/Component/Component.js
+++ docs/src/components/Component/Component.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import Markdown from './Markdown'
+import Markdown from '../Markdown'
 import './component.css'
 
 const Component = ({ children, readme }) =>
