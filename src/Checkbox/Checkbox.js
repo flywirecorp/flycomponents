2d3abc02f73d232bc7e6d681ea25422e5910c377
Vicent Gozalbes <vigosan@gmail.com>
2018-03-12 11:24:09 +0100

    Make Checkbox a class component

--- src/Checkbox/Checkbox.js
+++ src/Checkbox/Checkbox.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Checkbox = ({ className, id, name, error, label, ...otherProps }) => (
-  <label
-    htmlFor={id || name}
-    className={classNames('Checkbox', className, {
-      'has-error': error
-    })}
-  >
-    <input
-      name={name}
-      id={id || name}
-      className="Checkbox-input"
-      type="checkbox"
-      {...otherProps}
-    />
-    <span className="Checkbox-label">{label}</span>
-    {error && <div className="FormGroup-feedback">{error}</div>}
-  </label>
-);
+class Checkbox extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    error: PropTypes.string,
+    id: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  };
 
-Checkbox.propTypes = {
-  className: PropTypes.string,
-  error: PropTypes.string,
-  id: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
-};
+  render() {
+    const { className, id, name, error, label, ...otherProps } = this.props;
+
+    return (
+      <label
+        htmlFor={id || name}
+        className={classNames('Checkbox', className, {
+          'has-error': error
+        })}
+      >
+        <input
+          name={name}
+          id={id || name}
+          className="Checkbox-input"
+          type="checkbox"
+          {...otherProps}
+        />
+        <span className="Checkbox-label">{label}</span>
+        {error && <div className="FormGroup-feedback">{error}</div>}
+      </label>
+    );
+  }
+}
 
 export default Checkbox;
