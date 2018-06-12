9b41645c758aee4dbe19337693a923e27ff332d7
Manel Romero <manel@manelromero.com>
2018-06-12 15:26:46 +0200

    Add linter base file

--- src/MoneyInput/MoneyInput.js
+++ src/MoneyInput/MoneyInput.js
@@ -71,9 +71,7 @@ class MoneyInput extends Component {
 
   handleBlur = e => {
     const { onBlur } = this.props;
-    const {
-      target: { name, value: amount }
-    } = e;
+    const { target: { name, value: amount } } = e;
     const amountInCents = this.convertToCents(amount);
 
     onBlur(name, amountInCents);
@@ -89,10 +87,7 @@ class MoneyInput extends Component {
   };
 
   handleClick = e => {
-    const {
-      target,
-      target: { value }
-    } = e;
+    const { target, target: { value } } = e;
 
     target.setSelectionRange(0, value.length);
   };
