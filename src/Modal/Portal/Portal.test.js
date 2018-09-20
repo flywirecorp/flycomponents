1f71b418426bced9da5e9712d14e2f37e4040172
josecarlos.madrid <josecarlos.madrid@flywire.com>
2018-09-20 10:40:10 +0200

    [RD-2568] Fix body scroll

--- src/Modal/Portal/Portal.test.js
+++ src/Modal/Portal/Portal.test.js
@@ -19,12 +19,21 @@ describe('<Portal />', () => {
     );
   });
 
+  it('should position fixed to the document.body', () => {
+    document.body.innerHTML = '<div id="root"></div>';
+    ReactDOM.render(<Portal>Foo</Portal>, document.getElementById('root'));
+
+    expect(document.body.style.position).toBe('fixed');
+  });
+
   it('should append portal to a custom node', () => {
     document.body.innerHTML = '<div id="root"></div><div id="custom"></div>';
     ReactDOM.render(
       <Portal node={document.getElementById('custom')}>Foo</Portal>,
       document.getElementById('root')
     );
+
+    console.log(document.body.offsetWidth - document.body.clientWidth);
 
     expect(document.body.firstChild.outerHTML).toBe('<div id="root"></div>');
     expect(document.getElementById('custom').outerHTML).toBe(
