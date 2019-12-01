6169867cec5c9fa1e1af302136df3f51c067a756
Vicent Gozalbes <vigosan@gmail.com>
2019-12-01 09:47:13 +0100

    TBD

--- docs/src/components/Datepicker/Datepicker.js
+++ docs/src/components/Datepicker/Datepicker.js
@@ -9,40 +9,16 @@ export default () => (
       label="Date of Birth"
       locale="ES"
       name="birthday"
-      onChange={(name, date) => alert(`Your ${name} is the ${date}`)}
+      onChange={(name, date) => console.log(`Your ${name} is the ${date}`)}
       required
       value="04/21/1979"
     />
-    <Datepicker label="Pick a Date" locale="ES" name="floatingLabel" />
+
     <Datepicker
-      floatingLabel={false}
       label="Pick a Date"
       locale="ES"
-      name="other"
-      value="04/21/1979"
-    />
-    <Datepicker
-      label="Disabled Date Picker"
-      locale="ES"
-      name="disabled"
-      required
-      value="04/21/1979"
-      disabled
-    />
-    <Datepicker
-      label="Read Only Date Picker"
-      locale="ES"
-      name="readOnly"
-      required
-      value="04/21/1979"
-      readOnly
-    />
-    <Datepicker
-      label="Test Date Picker for reverse"
-      locale="ES"
-      name="test"
-      required
-      value="04/21/1979"
+      name="floatingLabel"
+      onChange={(name, date) => console.log(`Your ${name} is the ${date}`)}
     />
   </Component>
 );
