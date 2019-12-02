3beb6ee9233bff2009c1ddd0cdb6490659ef802e
Vicent Gozalbes <vigosan@gmail.com>
2019-12-02 11:20:31 +0100

    TBD

--- docs/src/components/Datepicker/Datepicker.js
+++ docs/src/components/Datepicker/Datepicker.js
@@ -6,17 +6,7 @@ import README from './README.md';
 export default () => (
   <Component readme={README}>
     <Datepicker
-      label="Date of Birth"
-      locale="ES"
-      name="birthday"
-      onChange={(name, date) => console.log(`Your ${name} is the ${date}`)}
-      required
-      value="04/21/1979"
-    />
-
-    <Datepicker
       label="Pick a Date"
-      locale="ES"
       name="floatingLabel"
       onChange={(name, date) => console.log(`Your ${name} is the ${date}`)}
     />
