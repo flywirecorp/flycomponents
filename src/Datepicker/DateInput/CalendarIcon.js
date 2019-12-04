65b171ab69cd4b3acf635be1d6d6a6d5455adc78
Vicent Gozalbes <vigosan@gmail.com>
2019-12-04 14:59:20 +0100

    Open calendar only when enter or space key pressed

--- src/Datepicker/DateInput/CalendarIcon.js
+++ src/Datepicker/DateInput/CalendarIcon.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { ENTER, SPACE } from '../../utils/keycodes';
 
 const LABEL = 'Show calendar';
 const CalendarIcon = ({ onClick, label }) => (
   <span
     className="Icon Icon--calendar"
     onClick={onClick}
-    onKeyDown={onClick}
+    onKeyDown={evt => [ENTER, SPACE].includes(evt.keyCode) && onClick(evt)}
     aria-label={label}
     role="button"
     tabIndex={0}
