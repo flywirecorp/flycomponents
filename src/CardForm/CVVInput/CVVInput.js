9dba6208d7ddf58b2161326e7a70c567c63a60b8
Jesus Lopez <jesus.lopez@flywire.com>
2021-09-21 17:35:17 +0200

    [PAAR-3329] Show and hide CVV tooltip on focus

--- src/CardForm/CVVInput/CVVInput.js
+++ src/CardForm/CVVInput/CVVInput.js
@@ -1,21 +1,38 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '../TextInput';
 
-const CVVInput = ({ cvvTooltip, className, ...props }) => {
+const CVVInput = ({ cvvTooltip, className, onFocus, onBlur, ...props }) => {
+  const [isTooltipVisible, showTooltip] = useState(false);
   const textInputClassName = classNames('cvvInput', className);
+
+  const handleFocus = () => {
+    showTooltip(true);
+    onFocus();
+  };
+
+  const handleBlur = () => {
+    showTooltip(false);
+    onBlur();
+  };
 
   return (
     <TextInput
       className={textInputClassName}
       ariaDescribedBy="cvvInput-Label-Tooltip"
+      onFocus={handleFocus}
+      onBlur={handleBlur}
       {...props}
     >
-      <span className="cvvInput-Icon-Tooltip" />
+      <span
+        className="cvvInput-Icon-Tooltip"
+        onMouseOver={() => showTooltip(true)}
+        onMouseOut={() => showTooltip(false)}
+      />
       <span
         id="cvvInput-Label-Tooltip"
-        className="cvvInput-Label-Tooltip"
+        className={`cvvInput-Label-Tooltip opacity-${isTooltipVisible ? 1 : 0}`}
         role="tooltip"
       >
         {cvvTooltip}
@@ -27,7 +44,14 @@ const CVVInput = ({ cvvTooltip, className, ...props }) => {
 CVVInput.propTypes = {
   className: PropTypes.string,
   cvvTooltip: PropTypes.string,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func
+};
+
+CVVInput.defaultProps = {
+  onBlur: () => {},
+  onFocus: () => {}
 };
 
 export default CVVInput;
