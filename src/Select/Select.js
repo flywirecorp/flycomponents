3d6d10f29358632dfc04703a8b268d631683f059
Vicent Gozalbes <vigosan@gmail.com>
2021-10-15 07:53:22 +0200

    Select fix warnings

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NOOP = () => {};
-const Select = ({
-  className,
-  disabled,
-  forwardRef,
-  name,
-  onChange = NOOP,
-  onClick = NOOP,
-  selectedValue,
-  values,
-  ...other
-}) => {
+const Select = ({ className, forwardRef, selectedValue, values, ...other }) => {
   const options = values.map(option => (
     <option key={option.value} value={option.value}>
       {option.label}
@@ -23,10 +12,6 @@ const Select = ({
   return (
     <select
       className={classToAdd}
-      disabled={disabled}
-      name={name}
-      onChange={onChange}
-      onClick={onClick}
       ref={forwardRef}
       value={selectedValue}
       {...other}
@@ -39,11 +24,7 @@ const Select = ({
 Select.displayName = 'Select';
 Select.propTypes = {
   className: PropTypes.string,
-  disabled: PropTypes.bool,
   forwardRef: PropTypes.object,
-  name: PropTypes.string,
-  onChange: PropTypes.func,
-  onClick: PropTypes.func,
   selectedValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   values: PropTypes.array.isRequired
 };
