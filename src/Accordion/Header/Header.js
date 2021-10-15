81ebf1c1239505ecfa53241cbcfc2ce57256eb93
Vicent Gozalbes <vigosan@gmail.com>
2021-10-15 07:46:42 +0200

    Accordion fix warnings
    - Add keyboard handles to header
    - Hides content when inactive

--- src/Accordion/Header/Header.js
+++ src/Accordion/Header/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ENTER, SPACE } from '../../utils/keycodes';
 
-const Header = ({ children, setActive }) => (
-  <header className="Accordion-sectionHeader" onClick={setActive}>
+const Header = ({ children, setActive, isActive }) => (
+  <div
+    className="Accordion-sectionHeader"
+    onClick={setActive}
+    onKeyDown={evt => [ENTER, SPACE].includes(evt.keyCode) && setActive()}
+    role="button"
+    aria-expanded={isActive}
+    tabIndex={0}
+  >
     {children}
-  </header>
+  </div>
 );
 
 Header.displayName = 'Header';
 Header.propTypes = {
   children: PropTypes.node,
+  isActive: PropTypes.bool,
   setActive: PropTypes.func
 };
 
