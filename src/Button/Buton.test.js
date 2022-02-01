f00f21ffd197c2066b45abf9a415303a9f005c1f
Vicent Gisbert <vicent.gisbert@flywire.com>
2022-02-01 16:11:10 +0100

    test improvements

--- src/Button/Buton.test.js
+++ src/Button/Buton.test.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import Button from './Button';
 import { fireEvent, render } from '@testing-library/react';
-import { async } from 'regenerator-runtime';
 
 describe('Button', () => {
   test('renders a button with "Submit" text by default', () => {
-    const  { getByText } = render(<Button/>);
+    const { getByText } = render(<Button />);
 
     expect(getByText(/submit/i)).toBeInTheDocument();
   });
 
   test('renders children in Button', () => {
     const children = 'Save';
-    const  { getByText } = render(<Button>{children}</Button>);
+
+    const { getByText } = render(<Button>{children}</Button>);
 
     expect(getByText(children)).toBeInTheDocument();
   });
 
   test('calls function onClick when click', () => {
     const onClick = jest.fn();
-    const  { getByText } = render(<Button onClick={onClick}/>);
 
+    const { getByText } = render(<Button onClick={onClick} />);
     fireEvent.click(getByText(/submit/i));
+
     expect(onClick).toHaveBeenCalled();
+  });
+
+  test('accepts other properties', () => {
+    const { getByText } = render(<Button disabled />);
+
+    expect(getByText(/submit/i)).toBeDisabled();
   });
 });
